a7c1cfd7bbed6f84fc2bac841d021a3d547d842f
yuri-mish <admin@starsam.net>
2020-12-08 17:59:32 +0200

    1

--- schema.js
+++ schema.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
 
+const { GraphQLJSON, GraphQLJSONObject } = require('graphql-type-json');
+
 const Authors = require('./data/authors');
 const Posts = require('./data/posts');
 
@@ -9,8 +11,41 @@ let {
     GraphQLObjectType,
     GraphQLNonNull,
     GraphQLSchema,
+    GraphQLFieldMap,
     GraphQLInt
 } = require('graphql');
+
+
+
+const PartnerType = new GraphQLObjectType({
+    name: "Partner",
+    description: "This represent Partner",
+    extensions:{
+        otk:{
+            key:'_id',
+            tbl:'cat'
+        }
+    },
+    fields: () =>{ console.log(this);return ({
+        _id: { type: GraphQLString},
+        ref: {type: GraphQLString},
+        name: { type: GraphQLString}
+        })
+    }
+});
+
+const BuyersOrderType = new GraphQLObjectType({
+    name: "BuyersOrder",
+    description: "This represent Buyers Order",
+    fields: () => ({
+            _id:{type:GraphQLString,resolve:(obj,args,cont)=>{
+                              return obj.jsb._id}},
+            jsb: {type:GraphQLJSONObject},   
+            number_doc:{type:GraphQLString},
+            partner:{type:PartnerType},       
+    })
+});
+
 
 const AuthorType = new GraphQLObjectType({
     name: "Author",
@@ -57,6 +92,35 @@ const BlogQueryRootType = new GraphQLObjectType({
                 return Posts
             }
         },
+        buyers_orders:{
+            name:'buyers_order',
+            type: new GraphQLList(BuyersOrderType),
+            args: {
+                ref : {
+                    type:GraphQLString
+                }
+            },
+            resolve: async function(par,arg,cont,info){
+//                console.log(rInfo.fieldNodes[0].selectionSet.selections)
+//const { fieldsList, fieldsMap,fieldsProjection } = require('graphql-fields-list');
+//console.log(fieldsList(info, { withDirectives: true }));       // [ 'id', 'firstName', 'lastName' ]
+//console.log(fieldsMap(info, { withDirectives: true }));        // { id: false, firstName: false, lastName: false }
+const fields = BuyersOrderType
+console.log(fields.getFields())
+//.map(f=>console.log(f.type))
+       
+                const dbf = require('./db')
+                res = await dbf.query("SELECT * FROM doc d  limit 10 ",[])
+                //, (err, res) => {
+                //         if (err) {
+                //           console.log(err)
+                //         }
+                //         console.log(res.rows)
+                //         res.fields.map((val)=>{console.log(val.name)})
+                //       })
+                return res.rows
+            }
+        },
         tst:{
             name:'fff',
             type:GraphQLString,
@@ -74,12 +138,9 @@ const BlogQueryRootType = new GraphQLObjectType({
                 
                     couch.changesReader.start({since:0,includeDocs:true})
                         .on('batch',b=>{
-                            
-             //               console.log(2)
                             b.map(rec=>{
                                 if(!rec.doc._id || !rec.doc.class_name) return
                                 var ref = rec.doc._id.split('|')[1]
-                                //console.log(rec,':',ref)
                                 var tbl_name = ''        
                                 switch (rec.doc.class_name.split('.')[0]){
                                     case 'doc':{
